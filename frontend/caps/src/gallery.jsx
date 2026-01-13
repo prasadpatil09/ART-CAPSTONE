4f109812b7c01585af8a2c7c7b5a86fac1cba69c
prasad patil <123prasad4009@sjcem.edu.in>
2026-01-13 22:23:57 +0530

    b

--- frontend/caps/src/gallery.jsx
+++ frontend/caps/src/gallery.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
-import './gallery.scss'
+import React, { useState } from "react";
+import "./gallery.scss";
+
 const Gallery = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const sf = [
+    "All Works",
+    "Digital Art",
+    "Black & White",
+    "Colorful",
+    "Canvas",
+  ];
+
   return (
     <div className="gallery">
-        <h1 className="gallery-title">Gallery Section</h1>
-        
-    </div>
-  )
-}
+      <div className="st">
+        <h1 className="gallery-title">CURATED COLLECTION</h1>
+        <h1 className="g-h1-1">Explore the Gallery</h1>
+        <p>
+          Browse through our collection of original artworks, each piece crafted
+          with passion <br />
+          and precision.
+        </p>
+      </div>
 
-export default Gallery
+      <div className="search-filter">
+        <ul className="fl-row">
+          {sf.map((item, index) => (
+            <li
+              key={index}
+              className={activeIndex === index ? "active" : ""}
+              onClick={() => setActiveIndex(index)}
+              onDoubleClick={() => setActiveIndex(null)}
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
